b9991bc81cc309c7fc76d2d485334c74e7b53baa
VIANCH Repository <vachavarro@gmail.com>
2012-09-04 11:56:34 -0500

    Functions declared
    functions to be used

--- vuploader.class.php
+++ vuploader.class.php
@@ -8,8 +8,73 @@
  * la teoria básica de un conector a MySQL desde PHP (se recomienda el uso de  MySQLi o PDO_MySQL)
  */
 
+/**
+ * if is active will print errors
+ */
+define('DEBUG',false);
+
+/**
+ * demo mode if is true simulates file uploads
+ */
+define('demo_mode', false);
+
 class vuploader{
-	
+
+   /**
+	* Allowed type of files
+	* @var array
+	*/
+	public $allowed_types = array('jpg','jpeg','png','gif');
+
+    /**
+     * dir where the files will be uploaded
+     * @var string
+     */
+	public $upload_dir = 'uploads/';
+
+	/**
+ 	 * Create a new class instance,
+ 	 * the params are allowed types to upload and
+ 	 * upload url, dir, folder on the server
+ 	 * Both parameters are optional
+ 	 *
+ 	 * @param array $allowed_types
+ 	 * @param string $upload_dir
+	 */
+	public function __construct($allowed_types = $this->allowed_types, $upload_dir = $this->upload_dir){
+
+		$this->allowed_types = $allowed_types;
+		$this->upload_dir = $upload_dir;
+		return true;
+	}
+
+	public function single_upload(){
+
+	}
+
+	public function multi_upload(){
+
+	}
+
+	public function delete_file(){
+
+	}
+
+	private function mk_dir(){
+
+	}
+
+	private function delete_dir(){
+		
+	}
+
+	private function printer(){
+
+	}
+
+	public function __destruct(){
+		return true;
+	}
 }
 ?>
 
