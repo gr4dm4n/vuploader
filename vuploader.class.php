72509d3a6e65c75ea8ba53643e6fb4941ddbfb4d
VIANCH Repository <vachavarro@gmail.com>
2012-09-04 10:59:40 -0500

    Creación del archivo clase
    Creditos y nombre de la case

---
+++ vuploader.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * VIANCH UPLOADER  Class
+ *
+ * @author Victor Chavarro {@link http://www.vianch.com Victor Chavarro (victor@vianch.com)}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * aunque el uso de la función nativa php mysql_connect esta obsoleto la utilizo para mostrar
+ * la teoria básica de un conector a MySQL desde PHP (se recomienda el uso de  MySQLi o PDO_MySQL)
+ */
+
+class vuploader{
+	
+}
+?>
+
